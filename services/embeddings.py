8da31d08ff230ad18f018d372c8724e2f9b47454
shreedevikalasaiah <shreedevikalasaiah@gmail.com>
2026-08-11 12:00:53 +0530

    Implement EmbeddingModel to load embeddings
    Added a new class to load embeddings using HuggingFace.

--- services/embeddings.py
+++ services/embeddings.py
@@ -1,1 +1,14 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from config import EMBEDDING_MODEL
 
+
+class EmbeddingModel:
+
+    @staticmethod
+    def load_embeddings():
+
+        embeddings = HuggingFaceEmbeddings(
+            model_name=EMBEDDING_MODEL
+        )
+
+        return embeddings
