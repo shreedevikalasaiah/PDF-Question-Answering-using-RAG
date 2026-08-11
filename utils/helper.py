d034ebdffd63c9749cc968b22ede64dcbc1f8191
shreedevikalasaiah <shreedevikalasaiah@gmail.com>
2026-08-11 12:10:12 +0530

    Update helper.py

--- utils/helper.py
+++ utils/helper.py
@@ -1,1 +1,16 @@
+import os
 
+from loaders.pdf_loader import load_pdf
+
+from vectorstore.vector_store import create_vectorstore
+
+from config import DB_PATH
+
+
+def initialize_database():
+
+    if not os.path.exists(DB_PATH):
+
+        docs = load_pdf("C:\Users\A1\RAG project\data\shree resume.pdf")
+
+        create_vectorstore(docs)
