43539a6865e8b8790eb8eac9dbf9e485bc0fdc84
shreedevikalasaiah <shreedevikalasaiah@gmail.com>
2026-08-11 12:00:19 +0530

    Implement RAG chain with document retrieval

--- chains/rag_chain.py
+++ chains/rag_chain.py
@@ -1,1 +1,26 @@
+from langchain.chains.combine_documents import create_stuff_documents_chain
 
+from langchain.chains import create_retrieval_chain
+
+from prompts.prompt import RAG_PROMPT
+
+from llm.llm import load_llm
+
+
+def get_rag_chain(vector_db):
+
+    retriever = vector_db.as_retriever(
+        search_kwargs={"k":3}
+    )
+
+    document_chain = create_stuff_documents_chain(
+        llm=load_llm(),
+        prompt=RAG_PROMPT
+    )
+
+    rag_chain = create_retrieval_chain(
+        retriever,
+        document_chain
+    )
+
+    return rag_chain
