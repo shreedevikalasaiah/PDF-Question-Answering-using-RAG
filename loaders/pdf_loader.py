87a234baf21bf133f2e0b05f033ecf7f472f667e
shreedevikalasaiah <shreedevikalasaiah@gmail.com>
2026-08-11 12:05:55 +0530

    Update pdf_loader.py

--- loaders/pdf_loader.py
+++ loaders/pdf_loader.py
@@ -1,1 +1,13 @@
+from langchain_community.document_loaders import PyPDFLoader
 
+
+class PDFLoader:
+
+    @staticmethod
+    def load_pdf(pdf_path: str):
+
+        loader = PyPDFLoader(pdf_path)
+
+        documents = loader.load()
+
+        return documents
