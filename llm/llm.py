6249ea4a200d75ce8ccfd510ceed1cacabc256f4
shreedevikalasaiah <shreedevikalasaiah@gmail.com>
2026-08-11 12:03:31 +0530

    Add load_llm function to initialize ChatGroq

--- llm/llm.py
+++ llm/llm.py
@@ -1,1 +1,13 @@
 
+from langchain_groq import ChatGroq
+
+from config import GROQ_API_KEY, LLM_MODEL
+
+
+def load_llm():
+
+    return ChatGroq(
+        model=LLM_MODEL,
+        api_key=GROQ_API_KEY,
+        temperature=0
+    )
