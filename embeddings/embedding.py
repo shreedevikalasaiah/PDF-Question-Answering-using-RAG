8c797eba19ce7c1dd387c4a53f9b8850b4c8ca63
shreedevikalasaiah <shreedevikalasaiah@gmail.com>
2026-08-11 12:02:23 +0530

    Add function to get embeddings using HuggingFace

--- embeddings/embedding.py
+++ embeddings/embedding.py
@@ -1,1 +1,8 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from config import EMBEDDING_MODEL
 
+def get_embedding():
+
+    return HuggingFaceEmbeddings(
+        model_name=EMBEDDING_MODEL
+    )
